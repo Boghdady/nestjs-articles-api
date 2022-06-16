00b5fbb6866e708d0de254b1443601e6e44f5fd9
boghdady <boghdady107@gmail.com>
2022-06-16 10:55:21 +0200

    Create migrations

---
+++ src/migrations/1655368917497-CreateTag.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTag1655368917497 implements MigrationInterface {
+  name = 'CreateTag1655368917497';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "tags" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "tags"`);
+  }
+}
