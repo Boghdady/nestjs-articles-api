6189c0841c5f49a6aefffb912360f1fa562244d8
boghdady <boghdady107@gmail.com>
2022-06-16 12:40:01 +0200

    Create User Dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
+import { CreateUserDto } from '@app/user/dtos/createUser.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  createUser(): string {
-    return this.userService.createUser();
+  createUser(@Body('user') createDto: CreateUserDto): CreateUserDto {
+    return this.userService.createUser(createDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@app/user/dtos/createUser.dto';
 
 @Injectable()
 export class UserService {
-  createUser(): string {
-    return 'Create User';
+  createUser(createUserDto: CreateUserDto): CreateUserDto {
+    return createUserDto;
   }
 }
