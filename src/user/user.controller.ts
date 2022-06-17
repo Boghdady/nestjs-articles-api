11bba7cf9d6d7565527f464023e7606678a1d43f
boghdady <boghdady107@gmail.com>
2022-06-17 16:08:34 +0200

    Create user and hash his password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dtos/createUser.dto';
+import { UserEntity } from '@app/user/user.entity';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  createUser(@Body('user') createDto: CreateUserDto): CreateUserDto {
+  createUser(@Body('user') createDto: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createDto);
   }
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,7 +1,29 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { hash } from 'bcrypt';
 
 @Entity({ name: 'users' })
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: '' })
+  bio: string;
+
+  @Column({ default: '' })
+  image: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    // salt -> 12 : how many time it will be encrypted
+    this.password = await hash(this.password, 12);
+  }
 }
